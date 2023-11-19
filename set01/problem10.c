899a751e58a39696c2a2ab40e9db8ea20a897dda
Ram <atheendrer.bsc23@rvu.edu.in>
2023-11-19 22:46:19 +0530

    committed 11/19/2023, 10:46:19 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -10,15 +10,18 @@ int stringcompare(char *string1, char *string2){//lol
     for(i=0;string1[i]!='\0' && string2[i]!='\0';i+=1){
         if (string1[i]>string2[i]){   
         return 0;
-          break;//test
     }
-    else if(string1[i]==string2[i]){
+    else if(string1[i]<string2[i]){
       return 1;
-      break;
     }
-    else
-      return 2;
-  }
+    }
+    if(string1[i]!='\0' && string2[i]=='\0'){
+      return 0;
+    }
+    else if(string1[i]=='\0' && string2[i]!='\0'){
+      return 1;
+    }
+  return 2;
 }
 
 void output(int result){
@@ -26,10 +29,10 @@ void output(int result){
         printf("str 1 is larger");
     }
     else if(result==1){
-        printf("str1 and 2 r same");
+        printf("str2 is larger");
     }
     else{
-        printf("str2 is larger");
+        printf("str 1 and 2 r the same");
     }
 }
 
