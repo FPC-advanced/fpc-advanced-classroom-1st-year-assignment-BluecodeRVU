5167e985397cf527a1f6a62fc38a584be3c18d18
Ram <atheendrer.bsc23@rvu.edu.in>
2023-12-02 17:40:23 +0530

    committed 12/2/2023, 5:40:22 PM

--- set02/problem03.c
+++ set02/problem03.c
@@ -17,7 +17,7 @@ int sqrrt(int s){
 
 int is_composite(int n){
   if(n!=0 && n!=1){
-    for(int i=2; i<=sqrt(n)+1; i++){
+    for(int i=2; i<=sqrrt(n)+1; i++){
       if(n%i==0){
         return 1;        
       }
