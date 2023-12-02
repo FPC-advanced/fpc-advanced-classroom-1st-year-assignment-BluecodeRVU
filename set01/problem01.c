ba31840b297d05fedae4cfb789360b91f26e4701
Ram <atheendrer.bsc23@rvu.edu.in>
2023-12-02 21:00:13 +0530

    committed 12/2/2023, 9:00:12 PM

--- set01/problem01.c
+++ set01/problem01.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
-int main()
-{
-    printf("hello world");
-return 0;
+int inp(){
+    printf("pls enter n: ");
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+    int a[100];
+        printf("Pls enter an array: ");
+        scanf("%d",a[i]);
+    }
+    return n;
+
 }
-
-
-
+int target(int n, int a[100]){
+    int t;
+    printf("pls enter target val:");
+    scanf("%d",&t);
+    for(int i=0;i<n;i++){
+        for(int j=1;j<n;j++){
+            if(a[i]+a[j]==t){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
