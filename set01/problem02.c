f3056ec59c7ff6f252d7445d8ae26ce3720d6803
Ram <atheendrer.bsc23@rvu.edu.in>
2023-12-03 16:09:53 +0530

    committed 12/3/2023, 4:09:52 PM

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+int inp(){
+  printf("pls enter n: ");
+  int n;
+  scanf("%d",&n);
+  return n;
+}
+void ainp(int n){
+  float a[100];
+  for(int i=0;i<n;i++){
+    printf("pls enter elements: ");
+    scanf("%f",&a[i]);
+  }
+}
+
+float med(int n,float a[100]){
+    if(n%2==0)
+    { float x1=a[n/2];
+    float x2=a[(n/2)+1];
+      float x=((x1+x2)/2.0);
+      return x;
+    }
+    else{
+      float x=(a[(n+1)/2]);
+      return x;
+    }
+}
+
 int main(){
-    int a=1;
-    int b=2;
-    int sum=a+b;
-    printf("%d",sum);}
+  int n=inp();
+  float a[100];
+  ainp(n);
+  float m=med(n,a);
+  printf("%f",m);
+}
