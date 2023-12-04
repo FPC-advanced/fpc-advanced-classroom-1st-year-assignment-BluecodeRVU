27c4cd101af967865430511d4344a7851faf6ad2
Ram <atheendrer.bsc23@rvu.edu.in>
2023-12-05 01:03:01 +0530

    committed 12/5/2023, 1:03:00 AM

--- set02/problem06.c
+++ set02/problem06.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-void inp(char str[]) {
+void inp(char str[100]) {
     printf("Enter a string: ");
     scanf("%s", str);
 }
 
-void rev(char str[]) {
+int rev(char str[100]) {
     int i, j;
-    char revstr[100];
     int lenstr = strlen(str);
-
-    for (j = 0, i = lenstr - 1; i >= 0; j++, i--) {
-        revstr[j] = str[i];  
+    for (j = lenstr, i = lenstr - 1; i >= 0; j++, i--) {
+        str[j] = str[i];  
     }
-    
+    return lenstr;
 
-    printf("Reversed string: %s\n", revstr);
+}
+
+void out(char str[100],int lenstr){
+    printf("Reversed string:");
+    for(int i=lenstr;i<2*lenstr;i++){
+        printf("%c",str[i]);
+    }
 }
 
 int main() {
     char str[100];
     inp(str);
-    rev(str);
+    int lenstr=rev(str);
+    out(str,lenstr);
     return 0;
 }
