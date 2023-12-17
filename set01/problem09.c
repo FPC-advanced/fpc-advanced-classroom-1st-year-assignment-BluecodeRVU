d739f717179640fc51da55a3b1f8163633e3b313
Ram <atheendrer.bsc23@rvu.edu.in>
2023-12-17 15:33:40 +0530

    committed 12/17/2023, 3:33:39 PM

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <math.h>
 float input()
 {   float n;
     printf("pls enter a num:");
@@ -8,11 +8,9 @@ float input()
 
 float square_root(float n){
     float x = 10;
-    while (x*x-n>0.01) {
+    while (fabs(x*x-n)>0.01) {
         x=0.5*(x + (n / x));
     }
- 
-//test
 return x;}
 
 void output(float sqrroot)
