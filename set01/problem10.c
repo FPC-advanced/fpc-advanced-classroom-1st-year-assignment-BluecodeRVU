5162e64ca545983f8ae4b69e4f3224c9559d4a5e
Ram <atheendrer.bsc23@rvu.edu.in>
2023-12-17 16:08:48 +0530

    committed 12/17/2023, 4:08:48 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -6,25 +6,20 @@ void input_two_strings(char *string1, char *string2){
     printf("enter the second str: ");
     scanf("%s",string2);
 }
-int stcmp(char *string1, char *string2){//lol
-    int i;
-    for(i=0;string1[i]!='\0' || string2[i]!='\0';i++){
-        if (string1[i]>string2[i]){   
-        return 1;
-    }
-    else if(string1[i]<string2[i]){
-      return 2;
-    }
+int stcmp(char *s1, char *s2){
+  int i;
+  for(i=0;s1[i]==s2[i] && s2[i]!='\0';i++){
+
   }
-  return 3;
+  return s1[i]-s2[i];
 }
 
 void output(int result){
-    if(result==1){
-        printf("str 1 is larger");
+    if(result<0){
+        printf("str 2 is larger ");
     }
-    else if(result==2){
-        printf("str2 is larger");
+    else if(result>0){
+        printf("str1 is larger ");
     }
     else{
         printf("str 1 and 2 r the same");
