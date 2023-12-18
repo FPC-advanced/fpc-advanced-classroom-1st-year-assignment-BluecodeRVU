8637b849be8ee85d733b40b63d70132e2be8bc02
Ram <atheendrer.bsc23@rvu.edu.in>
2023-12-18 14:49:35 +0530

    committed 12/18/2023, 2:49:35 PM

--- set05/problem06.c
+++ set05/problem06.c
@@ -1,47 +1,32 @@
 #include <stdio.h>
-
-int input_n() {
-    int n;
-    printf("Please enter a number: ");
-    scanf("%i", &n);
-    return n;
+int input_n(){
+  printf("enter the num of elements; ");
+  int n;
+  scanf("%d",&n);
+  return n;
+}
+void input(int n, int a[n]){
+  for(int i=0;i<n;i++){
+    scanf("%d",&a[i]);
+  }
 }
 
-void input(int n, int a[n]) {
-    int i;
-    for (i = 0; i < n; i++) {
-        printf("Input:");
-        scanf("%d", &a[i]);
-    }
+float odd_average(int n, int a[n]){
+  float sum=0;
+  float ele=0;
+  for(int i=0;i<n;i++){
+    (a[i]%2!=0)?(sum+=a[i]),ele++:0; 
+  }
+  return sum/ele;
 }
-
-float odd_average(int n, int a[n]) {
-    float avg;
-    float sum = 0;
-    int count = 0;
-    for (int i = 0; i < n; i++) {
-        if (a[i] % 2 != 0) {
-            count++;
-            sum += a[i];
-        }
-    }
-
-   
-        avg=sum/count; 
-
-    return avg;
+void output(float avg){
+  printf("%f",avg);
 }
-
-void output(float avg) {
-    printf("%f", avg);
-}
-
-int main() {
-    int n;
-    n = input_n();
-    int a[n];
-    input(n, a);
-    float avg = odd_average(n, a);
-    output(avg);
-    return 0;
+int main(){
+  int n=input_n();
+  int a[n];
+  input(n,a);
+  float avg=odd_average(n, a);
+  output(avg);
+  
 }
